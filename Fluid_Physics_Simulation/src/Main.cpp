36483cfcea670551471416104d039c8aa484ab7f
michaelangel007 <michaelangel007@sharedcraft.com>
2025-11-10 07:40:35 -0800

    Add proper command line options, added two benchmark modes.

--- Fluid_Physics_Simulation/src/Main.cpp
+++ Fluid_Physics_Simulation/src/Main.cpp
@@ -5,6 +5,15 @@
 #include <cmath>
 #include <limits> // MAX_INT
 
+static const char  *APP_NAME     = "Fluid Physics Simulation";
+static const char  *APP_VERSION  = "Version 1.1";
+
+// Configuration
+static bool   benchmark             = false;
+static bool   verbose               = false;
+static int    numFirstRenderFrame   = 0;
+static double numLastPhysicsSeconds = 0.0;
+
 // Defining static variables 
 std::vector <float> Window::recData = {
     -0.9f,  0.9f,
@@ -26,8 +35,137 @@ float Particle::pressureMultiplier = 200.0f;
 float Particle::nearPressureMultiplier = 1000.0f;
 float Particle::viscosityMultiplier = 0.0002f;
 
+void usage()
+{
+    const char *HELP =
+"-?              Display command line options and quit.\n"
+"--help          Alias for -?.\n"
+"-benchmark      Run simulation for 3 minutes (~10,800 frames @ 60fps), render first frame at frame number 7,200.\n";
+"-benchfast      Run simulation for 10 seconds (~600 frames @ 60fps), render first frame at frame number 300.\n";
+"-render #       Don't render until specified frame number. -1 is never render. (Default 0).\n"
+"-time   #.##    Run simulation for specified seconds.\n"
+"-v              Verbose mode off (default).\n"
+"+v              Verbose mode on.\n"
+"-V              Display version and quit.\n"
+"--version       Alias for -V.\n"
+"-vsync          VSync off.\n"
+"+vsync          VSync on (default).\n"
+    ;
+#if USE_CPP_IOSTREAM
+    std::cout << HELP;
+#else
+    printf( help );
+#endif
+}
+
+void version()
+{
+#if USE_CPP_IOSTREAM
+    std::cout
+        << APP_NAME    << std::endl
+        << APP_VERSION << std::endl;
+#else
+    printf( "%s\n%s\n", APP_NAME, APP_VERSION );
+#endif
+}
+
+void parseCommandLine(int nArgs, const char* aArgs[])
+{
+    const char *pArg = nullptr;
+    int         iArg = 1;
+
+    while (iArg < nArgs)
+    {
+        pArg = aArgs[ iArg ];
+        if (pArg[0] == '-')
+        {
+            if (strcmp(pArg, "-?") == 0) {
+                usage();
+                exit(0);
+            }
+            else
+            if (strcmp(pArg, "--help") == 0) {
+                usage();
+                exit(0);
+            }
+            else
+            if (strcmp(pArg, "-benchmark") == 0) {
+                numFirstRenderFrame   = 2*60 * 60; // 2 min * 60 s/min * 60 frames/s = 7,200 frames
+                numLastPhysicsSeconds = 3.0 * 60.0; // 3 min * 60 s/min = 180 seconds
+                benchmark = true;
+            }
+            else
+            if (strcmp(pArg, "-benchfast") == 0) {
+                numFirstRenderFrame   = 5 * 60; // 5 s * 60 frames/s = 300 frames
+                numLastPhysicsSeconds = 10.0; // 10 s
+                benchmark = true;
+            }
+            else
+            if (strcmp(pArg, "-render") == 0) {
+                iArg++;
+                if (iArg >= nArgs) {
+                    const char *ERROR = "ERROR: First frame to render was not specified.\ni.e.\n    -render 300\n";
+#if USE_CPP_IOSTREAM
+                    std::cout << ERROR;
+#else
+                    printf( ERROR );
+#endif
+                    exit(1);
+                }
+                pArg = aArgs[ iArg ];
+
+                numFirstRenderFrame = atoi( pArg );
+                if (numFirstRenderFrame < 0)
+                    numFirstRenderFrame = INT_MAX;
+            }
+            else
+            if (strcmp(pArg, "-time") == 0) {
+                iArg++;
+                if (iArg >= nArgs) {
+                    const char *ERROR = "ERROR: Time to run simulation was not specified.\ni.e.\n    -time 10.0\n";
+#if USE_CPP_IOSTREAM
+                    std::cout << ERROR;
+#else
+                    printf( ERROR );
+#endif
+                    exit(1);
+                }
+                pArg = aArgs[ iArg ];
+
+                numLastPhysicsSeconds = atof( pArg );
+                if (numLastPhysicsSeconds < 0.0)
+                    numLastPhysicsSeconds = 0.0;
+            }
+            else
+            if (strcmp(pArg, "-v") == 0) {
+                verbose = false;
+            }
+            else
+            if (strcmp(pArg, "-V") == 0) {
+                version();
+                exit(0);
+            }
+            else
+            if (strcmp(pArg, "--version") == 0) {
+                verbose = false;
+            }
+        }
+        else
+        if (pArg[0] == '+')
+        {
+            if (strcmp(pArg, "+v") == 0) {
+                verbose = true;
+            }
+        }
+
+        iArg++;
+    }
+}
+
 int main(int numArgs, const char *aArgs[])
 {
+    parseCommandLine( numArgs, aArgs );
+
     Window window(1600, 1000);
     
     // Generating Buffers
@@ -55,26 +193,10 @@ int main(int numArgs, const char *aArgs[])
 
     /* Loop until the user closes the window */
 
-    bool do_benchmark                   = numLastPhysicsSeconds > 0.0;
     static double lastTime              = 0.0f;
     static double elapsed               = 0.0;
     static int    numFrame              = 0;
-    static int    numFirstRenderFrame   = 2*60 * 60; // 2 min -> 2*60 -> 120 seconds * 60 fps/s = 7,200 frames
-    static double numLastPhysicsSeconds = 3.0 * 60.0; // 3 minutes * 60secs/min = 180 seconds
 
-    if (numArgs > 1)
-    {
-        if (numArgs > 1) {
-            numFirstRenderFrame = atoi( aArgs[1] );
-            if (numFirstRenderFrame < 0)
-                numFirstRenderFrame = INT_MAX;
-        }
-        if (numArgs > 2) {
-            numLastPhysicsSeconds = atof( aArgs[2] );
-            if (numLastPhysicsSeconds < 0.0)
-                numLastPhysicsSeconds = 0.0;
-        }
-    }
 #if USE_CPP_IOSTREAM
     std::cout.precision(6);
     std::cout
@@ -103,22 +225,26 @@ int main(int numArgs, const char *aArgs[])
         double deltaTime = currentTime - lastTime;
                elapsed += deltaTime; // POHO
         lastTime = currentTime;
+
+        if (verbose) {
 #if USE_CPP_IOSTREAM
-        std::cout
-            << "FPS: "          << std::setw(7) << std::setprecision(3) << (1.f / deltaTime)
-            << " / Frametime: " << std::setw(7) << std::setprecision(3) << deltaTime * 1000.f << "ms"
-            << "  Frame #: "    << std::setw(7)                         << numFrame
-            << "  Elapsed: "    << std::setw(7) << std::setprecision(3) << elapsed << " s"
-            << std::endl;
+            std::cout
+                << "FPS: "          << std::setw(7) << std::setprecision(3) << (1.f / deltaTime)
+                << " / Frametime: " << std::setw(7) << std::setprecision(3) << deltaTime * 1000.f << "ms"
+                << "  Frame #: "    << std::setw(7)                         << numFrame
+                << "  Elapsed: "    << std::setw(7) << std::setprecision(3) << elapsed << " s"
+                << std::endl;
 #else
-        printf( "FPS: %7.3f / Frametime: %7.3f ms  Frame #: %7d  Elapsed: %7.3f s\n", (1.f / deltaTime), deltaTime * 1000.f, numFrame, elapsed );
+            printf( "FPS: %7.3f / Frametime: %7.3f ms  Frame #: %7d  Elapsed: %7.3f s\n", (1.f / deltaTime), deltaTime * 1000.f, numFrame, elapsed );
 #endif
+        }
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window.win);
 
         /* Poll for and process events */
         glfwPollEvents();
-        if (do_benchmark && (elapsed >= numLastPhysicsSeconds)) break;
+        if (benchmark && (elapsed >= numLastPhysicsSeconds)) break;
     }
 
     double frames  = (double)numFrame; // frames
